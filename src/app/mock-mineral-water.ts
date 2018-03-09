28c7d0facd8d8386dfcd4323e614c92546f14031
Phillip Rak <rak.phillip@gmail.com>
2018-03-09 15:38:07 -0600

    --Populated mock mineral water data

---
+++ src/app/mock-mineral-water.ts
@@ -0,0 +1,54 @@
+import {mineralWater} from '../models/mineralWater';
+
+export const waterList: mineralWater[] = [
+  new mineralWater(
+    0,
+    'Acqua Panna',
+    30.2,
+    6.9,
+    6.5,
+    0.9,
+    100,
+    21.4,
+    7.1,
+    5.7,
+    0.008,
+    8.2,
+    137,
+    '',
+    ''
+  )
+  , new mineralWater(
+    1,
+    'Antipodes',
+    3.5,
+    1.8,
+    11,
+    3.3,
+    35,
+    4.5,
+    7,
+    1.5,
+    6.5,
+    130,
+    68,
+    'Used for strong, pale, bitter ales',
+    ''
+  )
+  , new mineralWater(
+    2,
+    'Apollinaris',
+    100,
+    130,
+    410,
+    20,
+    1810,
+    80,
+    100,
+    1.6,
+    2767,
+    2,652,
+    'Used for dark, soft flavored beers',
+    ''
+  )
+];
