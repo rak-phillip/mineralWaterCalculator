c13a589f3db67afad22c03500a32cb90f115a054
Phillip Rak <rak.phillip@gmail.com>
2018-03-09 14:42:04 -0600

    --Updated names to common for readability
      At least this is easier for me, will be open to change if good
      argument is made

--- src/models/mineralWater.ts
+++ src/models/mineralWater.ts
@@ -1,30 +1,80 @@
 export class mineralWater {
   id: number;
   name: string;
-  ca: number;
-  mg: number;
-  na: number;
-  k: number;
-  hco3: number;
-  so4: number;
-  cl: number;
-  no3: number;
+  calcium: number;
+  magnesium: number;
+  sodium: number;
+  potassium: number;
+  bicarbonate: number;
+  sulfate: number;
+  chloride: number;
+  nitrate: number;
   co2: number;
   pH: number;
   TDS: number;
   comment: string;
   url: string;
 
-  private TDSArr = [this.ca, this.mg, this.na, this.k, this.hco3, this.so4, this.cl, this.no3];
+  private TDSArr = [
+    this.calcium
+    , this.magnesium
+    , this.sodium
+    , this.potassium
+    , this.bicarbonate
+    , this.sulfate
+    , this.chloride
+    , this.nitrate
+  ];
+
+  /**
+   * @function TDSCalculated()
+   * @description A calculation of Total Dissolved Solids. Sums items in Total Dissolved Solids array.
+   * @returns {number}
+   * @constructor
+   */
   TDSCalculated = () => this.TDSArr.reduce((i, j) => i + j);
+
+  /**
+   * @function TDSDiff()
+   * @description Gets the difference between Total Dissolved Solids and Calculated value.
+   * @returns {number}
+   * @constructor
+   */
   TDSDiff = () => Math.abs(this.TDS - this.TDSCalculated());
-  caByMg = () => this.ca / this.mg;
-  caByNa = () => this.ca / this.na;
-  positiveCharge = () => (2 * (this.ca / 40.078))
-    + (2 * (this.mg / 24.305))
-    + (this.na / 22.99)
-    + (this.k / 39.098);
+
+  /**
+   * @function caByMg()
+   * @description The ratio of Calcium to Magnesium
+   * @returns {number}
+   */
+  caByMg = () => this.calcium / this.magnesium;
+
+  /**
+   * @function caByNa()
+   * @description The ration of Calcium to Sodium
+   * @returns {number}
+   */
+  caByNa = () => this.calcium / this.sodium;
+
+
+  /**
+   * @positiveCharge()
+   * @description Calculates the positive charge for solution
+   * @returns {number}
+   */
+  positiveCharge = () => (2 * (this.calcium / 40.078))
+    + (2 * (this.magnesium / 24.305))
+    + (this.sodium / 22.99)
+    + (this.potassium / 39.098);
+
+  /**
+   * @description Calculates the negative charge for solution
+   */
   negativeCharge: number;
+
+  /**
+   * @description Calculates the difference between charges for solution
+   */
   chargeDiff: number;
 }
 
