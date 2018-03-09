a34d5a582892f70bb236abc736daf42b86f9000b
Phillip Rak <rak.phillip@gmail.com>
2018-03-09 07:30:23 -0600

    --Created TODO list of potential features
      Starting focus on listing recipes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,13 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+
+  // TODO: Save Recipes
+  // TODO: List Recipes
+  // TODO: Create Recipes
+  // TODO: Add Credits
 
   constructor(public navCtrl: NavController) {
 
