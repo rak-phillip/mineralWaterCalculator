e1c331c88dd78867a542f382e5e180511ecb897a
Phillip Rak <rak.phillip@gmail.com>
2018-03-09 15:38:23 -0600

    --Populated list with mock mineral water data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {waterList} from "../../app/mock-mineral-water";
+import {mineralWater} from "../../models/mineralWater";
 
 @Component({
   selector: 'page-home',
@@ -17,6 +19,7 @@ export class HomePage {
   // TODO: Add links to ingredients? How can we reduce the barrier of entry to allowing people to purchase?:w
 
   waterTemplate;
+  water: mineralWater[] = waterList;
 
   constructor(public navCtrl: NavController) {
 
